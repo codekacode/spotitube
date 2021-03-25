1f5d6fbf9cd6cf48cbfe4dffcbb80620f0161aa8
bllerenac <bllerenac@uni.pe>
2021-03-25 13:55:02 -0500

    add input type range

--- src/Components/Contents/Player.js
+++ src/Components/Contents/Player.js
@@ -1,8 +1,26 @@
-//import { css } from "@emotion/react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import test from "../../assets/test/say_so.mp3"
 import Icons from "../UI/Icons"
 import Colors from "../UI/Colors"
+
+const StyledPlayer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  gap: 12px;
+`;
+
+const StyledControl = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+`;
 
 const StyledIcon = styled.img`
   width: 14px;
@@ -22,17 +40,17 @@ const StyledPlay = styled.div`
       width: 10.7px;
       height: 12.32px;
     }
+  &:hover{
+    transform: scale(1.1);
+  }
 `;
 
-const StyledControl = styled.div`
+const ProgressBar = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  gap: 32px;
 `;
 
-const onClick = (e) =>{
+const PlayPause = (e) =>{
   const play = e.currentTarget
   const audio = document.getElementById("audio")
   if(audio.paused || audio.ended){
@@ -48,19 +66,39 @@ const onClick = (e) =>{
 
 function Player(){
   return(
-    <div>
+    <StyledPlayer>
         <StyledControl>
           <StyledIcon alt="prev.svg" src={Icons.prev}/>
-          <StyledPlay id="controls" onClick={onClick}>
+          <StyledPlay id="controls" onClick={PlayPause}>
             <StyledIcon alt="play_filled.svg" src={Icons.play_filled} className="play"/>
             <StyledIcon alt="play_filled.svg" className="hide pause" src={Icons.stop}/>
           </StyledPlay>
           <StyledIcon alt="next.svg" src={Icons.next}/>
         </StyledControl>
+        <ProgressBar>
+          <p>00:00</p>
+          <input type="range" min="0" max="100" 
+              css={css`
+                -webkit-appearance: none;
+                height: 6px;
+                width: 100%;
+                border-radius: 5px;
+                outline: none;
+                  &::-webkit-slider-thumb{
+                    -webkit-appearance: none;
+                    width: 48px;
+                    height: 48px;
+                    cursor: pointer;
+                    z-index: 3;
+                    position: relative;
+                  }
+              `}/>
+          <p>00:00</p>
+        </ProgressBar>  
         <audio controls id="audio" className="hide">
           <source src={test} type="audio/mp3"/>
         </audio>
-    </div>
+    </StyledPlayer>
   );
 }
 
