db2977beeb4b814d8a24f8feca2150ba2d291b37
susanluisa <susanluisacoari@gmail.com>
2021-03-25 16:34:05 -0500

    Created button sign

---
+++ src/components/signComponents/ButtonSign.js
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+
+
+const StyledButton = styled.button`
+  border-radius: 500px;
+  border: 1px solid #616467;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 48px;
+  width: 450px;
+  min-width: 160px;
+  &:hover{
+    color: #FFFFFF;
+    background-color: ${(props) => (props.color)};
+  }
+`; 
+
+function ButtonSign({color='#616467', children}){
+
+  return(
+    <StyledButton color={color}>
+      {children}
+    </StyledButton>
+  )
+};
+
+export default ButtonSign;
