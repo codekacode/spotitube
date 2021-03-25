da5292e202c0ef669e16469825e08c622005e76c
Codekacode <karem.quiroz.m@gmail.com>
2021-03-25 12:12:22 -0500

    Add distribution and fix bugs in navbar

--- src/components/contents/NavBar.js
+++ src/components/contents/NavBar.js
@@ -1,20 +1,10 @@
 import ButtonNav from "../ui/ButtonNav"
 import styled from "@emotion/styled";
 
-const StyledDiv = styled.div`
-  display: flex;
-  height: 100vh;
-  flex-direction: column;
-  background: #000;
-  flex: 0.2;
-`;
-const List = styled.p`
-  color: white;
-`;
-
 function NavBar(){
   return(
     <StyledDiv>
+      <img className="navbar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"/>
       <ButtonNav label="Inicio" icon="home"/>
       <ButtonNav label="Buscar" icon="search_small"/>
       <ButtonNav label="Premium" icon="credit"/>
@@ -24,3 +14,25 @@ function NavBar(){
 }
 
 export default NavBar;
+
+const StyledDiv = styled.div`
+  display: flex;
+  height: 100vh;
+  min-width: 230px;
+  flex-direction: column;
+  background: #040404;
+  color: #fff;
+  flex: 0.2;
+  & .navbar__logo{
+    margin-right: auto;
+    object-fit: contain;
+    height: 85px;
+    padding: 10px;
+  }
+`;
+const List = styled.p`
+  margin-top: 50px;
+  margin-left: 10px;
+  padding: 5px;
+  font-size: 16px;
+`;
