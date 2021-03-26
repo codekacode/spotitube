a0bc01329b43d23b127ed7527b4616fb310b48b4
Bllerenac <66619178+bllerenac@users.noreply.github.com>
2021-03-26 12:31:54 -0500

    Revert "add bse_URL ans test and test _add data"

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,2 @@
-export const KEY_API = "AIzaSyCd1TUBs-9Vr8vh33vRE2LI6fYQFFECnxo";
-export const BASE_URI = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=";
-export const TEST = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=the weekend&key=AIzaSyCd1TUBs-9Vr8vh33vRE2LI6fYQFFECnxo&maxResults=25";
-//panel de prueba de la api en esta url https://developers.google.com/youtube/v3/docs/search/list?apix=true
-//pueden probarla y ver como llama y recibe la informacion
+KEY_API= "AIzaSyCd1TUBs-9Vr8vh33vRE2LI6fYQFFECnxo";
+//panel de p
--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import videosReducer from "../features/videoList/videoSlice";
-
-
-export default configureStore({
-  reducer: {
-    videos: videosReducer,
-  },
-});
