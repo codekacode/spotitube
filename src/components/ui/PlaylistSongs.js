e1cbbbf5ac3a54fceb0d6f7064ec62662175c730
bllerenac <bllerenac@uni.pe>
2021-03-28 14:22:54 -0500

    modificated songs

--- src/components/ui/PlaylistSongs.js
+++ src/components/ui/PlaylistSongs.js
@@ -1,92 +1,39 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import styled from '@emotion/styled';
-import Icons from "./Icons";
+//import Icons from "./Icons";
 //import Colors from "./Colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-//import {AiOutlineHeart} from 'react-icons/ai'
+//import FavoriteIcon from "@material-ui/icons/Favorite";
+//import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import {WiTime2} from 'react-icons/wi'
 import SongList from './SongList';
+//import Album from "../../assets/images/test_album.png"
 
-const PlayPause = (e) =>{
-  const play = e.currentTarget
-  const play_footer = document.getElementById("controls_footer");
-  const audio = document.getElementById("audio");
-  const id = document.getElementById("song_id");
-  const equal = document.getElementById("song_equal");
-  if(audio.paused || audio.ended){
-    audio.play();
-    play.querySelector(".play").classList.toggle("hide");
-    play.querySelector(".pause").classList.toggle("hide");
-    play_footer.querySelector(".play").classList.toggle("hide");
-    play_footer.querySelector(".pause").classList.toggle("hide");
-    id.classList.toggle("hide");
-    equal.classList.toggle("hide");
-  }else{
-    audio.pause();
-    play.querySelector(".play").classList.toggle("hide");
-    play.querySelector(".pause").classList.toggle("hide");
-    play_footer.querySelector(".play").classList.toggle("hide");
-    play_footer.querySelector(".pause").classList.toggle("hide");
-    id.classList.toggle("hide");
-    equal.classList.toggle("hide");
-  }
-}
+const TitlesContainer = styled.div`
+    display: grid;
+    padding: 0 16px;
+    grid-gap: 16px;
+    grid-template-columns: [index] 16px [first] 4fr [var1] 2fr [last] minmax(120px,1fr);
+    height: 56px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    position: relative;
+      & p{
+        justify-self: start;
+      }
+`
 
 export default function PlaylistSongs() {
   return (
-    <StyledSong>
-      <SongId>
-        <p id="song_id">1</p>
-        <img alt="equal.img" css={css`width: 14px; height: 14px;`} id="song_equal" src="https://open.scdn.co/cdn/images/equaliser-animated-green.73b73928.gif" className="hide"/>
-        <StyledPlay id="controls_song" onClick={PlayPause}>
-          <img alt="play_filled.svg" src={Icons.play_filled} className="play"/>
-          <img alt="play_filled.svg" className="pause hide" src={Icons.stop}/>
-        </StyledPlay>
-      </SongId>
-      <FavoriteIcon fontSize="large" css={css`width:16px; height:16px;`}/>
-      <MoreHorizIcon />
-      <SongList />
-    </StyledSong>
+    <div>
+      <TitlesContainer>
+        <p>#</p>
+        <p>Titulo</p>
+        <p>ÁLBUM</p>
+        <p>Agregado el</p>
+        <WiTime2 css={css`width:16px; height:16px;`}/>
+      </TitlesContainer>
+      <SongList/>
+    </div>
   )
 }
-
-const StyledSong = styled.div`
-  margin: 30px 30px;
-  display: flex;
-  align-items: center;
-  gap: 20px;
-  &:hover{
-    & #song_id, #song_equal{
-      display: none;
-    }
-    & #controls_song{
-      display: flex;
-    }
-  }
-`;
-
-const SongId = styled.div`
-  & #song_id{
-    text-align: center;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 24px;
-    letter-spacing: normal;
-    text-transform: none;
-  }
-`;
-
-const StyledPlay = styled.div`
-  display: none;
-  align-items: center;
-  justify-content: center;
-  width: 16px;
-  height: 16px;
-  border-radius: 50%;
-  box-sizing: border-box;
-  & img {
-    width: 16px;
-    height: 16px;
-  }
-`;
