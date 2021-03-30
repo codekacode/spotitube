d0381af0ef8e12437d6d1353512c1543fd25dcd3
bllerenac <bllerenac@uni.pe>
2021-03-30 02:16:11 -0500

    add navlink to nav bar

--- src/components/ui/ButtonNav.js
+++ src/components/ui/ButtonNav.js
@@ -1,17 +1,17 @@
 /** @jsxImportSource @emotion/react */
 import Icons from "./Icons"
 import styled from "@emotion/styled";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 import Colors from "./Colors"
 
 function ButtonNav({label, icon, link_to}){
   return(
     <StyledDiv>
-      <Link to={link_to}>
+      <NavLink exact to={link_to} activeClassName="selected">
       <img alt={`icon_`+ icon} src={Icons[icon]}/>
         {label}
-      </Link>
+      </NavLink>
     </StyledDiv>
   );
 }
@@ -38,13 +38,18 @@ const StyledDiv = styled.div`
     gap: 10px;
     padding: 8px 20px;
     text-decoration: none;
+    :hover{
+    color:white;
+    border-left: 4px solid ${Colors.primary};
+    }
   }
 
   & img{
     width: 20.5px;
     height: 22.8px;
   }
-  &:hover{
+  
+  & .selected{
     color:white;
     border-left: 4px solid ${Colors.primary};
   }
