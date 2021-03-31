6e583c866c70ea85633e5ff10b933d0bb5dd98be
bllerenac <bllerenac@uni.pe>
2021-03-30 22:54:11 -0500

    header_component

--- src/pages/PlayList.js
+++ src/pages/PlayList.js
@@ -5,22 +5,21 @@ import PlaylistSongs from '../components/contents/PlaylistSongs';
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: column;
-    background-color: red;
     width: 100%;
     height: 100%;
     overflow: auto;
 `;
-const TestDiv = styled.div`
-    height: 232px;
-    width: 100%;
-    float: left;
-    background-color: rgb(216, 40, 48);
-    background-clip: padding-box;
-    background-image: linear-gradient(rgba(0,0,0,.6),#121212);
-`;
+//const TestDiv = styled.div`
+//    height: 232px;
+//    width: 100%;
+//    float: left;
+//    background-color: rgb(216, 40, 48);
+//    background-clip: padding-box;
+//    background-image: linear-gradient(rgba(0,0,0,.6),#121212);
+//`;
 
 const DivContent = styled.div`
-    height: 100%;
+    height: auto;
     background: #121212;
     isolation: isolate;
 `;
